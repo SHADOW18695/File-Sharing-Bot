0c9ae9d21694a9ea6ee3df96457c48c15c8744a2
SHADOW18695 <99009597+SHADOW18695@users.noreply.github.com>
2024-08-21 21:29:51 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>🤖 My Name :</b> <a href='https://t.me/FileSharingXProBot'>File Sharing Bot</a> \n<b>📝 Language :</b> <a href='https://python.org'>Python 3</a> \n<b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram {__version__}</a> \n<b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a> \n<b>📢 Channel :</b> <a href='https://t.me/Madflix_Bots'>Madflix Botz</a> \n<b>🧑‍💻 Developer :</b> <a href='tg://user?id={OWNER_ID}'>Jishu Developer</a>",
+            text = f"<b>‣ My Name :</b> <a href='https://t.me/filesharecinemabot'>File Sharing Movie Bot</a> \n<b>‣ Language :</b> <a href='https://python.org'>Python 3</a> \n<b>‣ Library :</b> <a href='https://pyrogram.org'>Pyrogram {__version__}</a> \n<b>‣ Server :</b> <a href='https://koyeb.com'>Koyeb</a> \n<b>‣ Channel :</b> <a href='https://t.me/cinemahdmovie'>CINEMA HUB</a> \n<b>‣ Maintained By :</b> <a href='https://t.me/Movie_Request_233bot'>CINEMA HUB</a>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
